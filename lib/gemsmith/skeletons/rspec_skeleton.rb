cdd6ea903124c656b7d0ca702867b667c8865f73
Paul Sadauskas <psadauskas@gmail.com>
2016-08-29 11:19:47 -0600

    Use the standard spec_helper.rb generated by rspec
    This makes it easier to upgrade rspec in projects, fewer changes to merge.

--- lib/gemsmith/skeletons/rspec_skeleton.rb
+++ lib/gemsmith/skeletons/rspec_skeleton.rb
@@ -10,10 +10,6 @@ module Gemsmith
         cli.template "%gem_name%/lib/tasks/rspec.rake.tt", configuration.to_h
         cli.template "#{rspec_root}/spec_helper.rb.tt", configuration.to_h
         cli.template "#{rspec_root}/lib/%gem_name%/%gem_name%_spec.rb.tt", configuration.to_h
-        cli.template "#{rspec_root}/support/kit/default_config.rb.tt", configuration.to_h
-        cli.template "#{rspec_root}/support/kit/stderr.rb.tt", configuration.to_h
-        cli.template "#{rspec_root}/support/kit/stdout.rb.tt", configuration.to_h
-        cli.template "#{rspec_root}/support/kit/temp_dir.rb.tt", configuration.to_h
       end
 
       private
